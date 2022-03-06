50a4f9c4c94cb3c5d87d9067109cc977d96c795f
KangarooFist <damienbonello97@gmail.com>
2022-03-06 22:46:25 +0100

    Enabled BrowserRouter

--- Blockchain_Web3/client/src/App.jsx
+++ Blockchain_Web3/client/src/App.jsx
@@ -1,17 +1,26 @@
-import {Navbar, Footer, Services, Transactions, Welcome, CryptoCard} from './components';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { Navbar } from './components';
+import DEX from './pages/DEX';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import Tutorials from './pages/Tutorials';
 
 const App = () => {
   return (
-    <div className="min-h-screen overflow-hidden">
-      <div className='gradient-bg-welcome'>
+    <BrowserRouter>
+      <div className="overflow-hidden gradient-bg-navbar">
         <Navbar />
-        <Welcome />
-        <CryptoCard />
       </div>
-      <Services />
-      <Transactions />
-      <Footer /> 
-    </div>
+               
+      <Routes>
+        <Route index element={<Home />} />
+          <Route path="/DEX" element={<DEX />} />
+          <Route path="/Portfolio" element={<Portfolio />} />
+          <Route path="/Tutorials" element={<Tutorials />} />
+      </Routes>
+      
+    
+    </BrowserRouter>
   );
 }
 
